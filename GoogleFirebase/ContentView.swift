ff56497fd7bc1b701deef60168622dfc0fb3bda7
Max <maxim.macari.dam@gmail.com>
2020-10-05 17:29:01 +0200

    Initial Commit

---
+++ GoogleFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoogleFirebase
+//
+//  Created by Maxim Macari on 05/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoogleFirebase/GoogleFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleFirebaseApp.swift
+//  GoogleFirebase
+//
+//  Created by Maxim Macari on 05/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GoogleFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
